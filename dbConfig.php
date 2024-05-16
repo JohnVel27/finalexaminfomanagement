7d6dc44f86f494b2f1c6f5d9024b6ced21e3d82f
JohnVel27 <124770839+JohnVel27@users.noreply.github.com>
2024-05-15 22:59:46 -0700

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -11,15 +11,4 @@ $conn->exec("SET time_zone = '+08:00';");
 if(mysqli_connect_error()){
     echo 'error';
 }
-
-try {
-    // SQL statement to alter the table
-    $sql = "ALTER TABLE login MODIFY COLUMN id INT AUTO_INCREMENT";
-
-    // Prepare and execute the SQL statement
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-} catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
 ?>
